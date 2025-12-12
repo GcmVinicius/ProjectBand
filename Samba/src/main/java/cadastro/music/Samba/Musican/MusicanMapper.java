0f89cf10188a423d6d1eb63f95b64cb0ac237aeb
Vinicius Machado <viniciusgabrielcoutomachado@gmail.com>
2025-12-12 13:24:30 -0300

    feat: Implements the funcionality of 'MusicanMapper'

--- Samba/src/main/java/cadastro/music/Samba/Musican/MusicanMapper.java
+++ Samba/src/main/java/cadastro/music/Samba/Musican/MusicanMapper.java
@@ -1,4 +1,39 @@
 package cadastro.music.Samba.Musican;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class MusicanMapper {
+    public MusicanModel map(MusicanDTO musicanDTO) {
+        if (musicanDTO == null) {
+            return null;
+        }
+
+        MusicanModel musicanModel = new MusicanModel();
+
+        musicanModel.setId(musicanDTO.getId());
+        musicanModel.setName(musicanDTO.getName());
+        musicanModel.setImage(musicanDTO.getImage());
+        musicanModel.setContact(musicanDTO.getContact());
+        musicanModel.setInstrument(musicanDTO.getInstrument());
+
+        return musicanModel;
+    }
+
+    public MusicanDTO map(MusicanModel musicanModel) {
+        if (musicanModel == null) {
+            return null;
+        }
+
+        MusicanDTO musicanDTO = new MusicanDTO();
+
+        musicanDTO.setId(musicanModel.getId());
+        musicanDTO.setName(musicanModel.getName());
+        musicanDTO.setImage(musicanModel.getImage());
+        musicanDTO.setContact(musicanModel.getContact());
+        musicanDTO.setInstrument(musicanModel.getInstrument());
+
+        return musicanDTO;
+    }
 }
